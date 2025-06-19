8dd7603a781a388e91f3764ab70ddc113987d037
s31830 <s31830@pjwstk.edu.pl>
2025-06-19 21:27:03 +0200

    template

---
+++ WebApplication1/WebApplication1/Controllers/Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Controller(IDbService Service):ControllerBase
+{
+    // [HttpGet]
+    // public IActionResult Get()
+    // {
+    //     // return Ok(await Service);
+    // }
+}
---
+++ WebApplication1/WebApplication1/Services/DbService.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Globalization;
+using Microsoft.Data.SqlClient;
+
+
+namespace WebApplication1.Services;
+
+public interface IDbService
+{
+    // public Task<ICollection<PolitykGetDto>> GetPolitycy();
+    // public Task<PolitykGetDto>CreatePolityk(PolitykCreateDto polityk);
+}
+public class DbService(IConfiguration config):IDbService
+{
+   
+        
+}
